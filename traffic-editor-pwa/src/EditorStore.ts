f0f175cde310b13d34417f645d69da736ad00ac4
Morgan Quigley <morgan@osrfoundation.org>
2021-06-21 00:07:24 +0800

    refactor yaml emitters into classes
    Signed-off-by: Morgan Quigley <morgan@osrfoundation.org>

--- traffic-editor-pwa/src/EditorStore.ts
+++ traffic-editor-pwa/src/EditorStore.ts
@@ -4,28 +4,43 @@ import * as THREE from 'three';
 import { v4 as generate_uuid } from 'uuid'
 import YAML from 'yaml'
 
-export interface EditorParam {
-  type_idx: number,
-  name: string,
-  value: any,
-  uuid: string
+export class EditorParam {
+  type_idx: number = 0;
+  name: string = '';
+  value: any = 0;
+  uuid: string = '';
+
+  static fromYAML(_name: string, data: any): EditorParam {
+    let p = new EditorParam();
+    p.name = _name;
+    p.type_idx = data[0];
+    p.value = data[1];
+    p.uuid = generate_uuid();
+    return p;
+  }
 }
 
 export class EditorObject {
   uuid: string = '';
   params: EditorParam[] = [];
 
-  params_from_yaml(params_yaml: any): void {
+  paramsFromYAML(params_yaml: any): void {
     for (const param_name in params_yaml) {
-      const param_data = params_yaml[param_name];
-      let p: EditorParam = {
-        name: param_name,
-        type_idx: param_data[0],
-        value: param_data[1],
-        uuid: generate_uuid(),
-      };
-      this.params.push(p);
+      this.params.push(EditorParam.fromYAML(param_name, params_yaml[param_name]));
     }
+  }
+
+  paramsToYAML(): YAML.YAMLMap {
+    let node = new YAML.YAMLMap();
+    for (const param of this.params) {
+      let param_value_node = new YAML.YAMLSeq();
+      param_value_node.add(param.type_idx);
+      param_value_node.add(param.value);
+      param_value_node.flow = true;
+      node.add({ key: param.name, value: param_value_node });
+    }
+    node.flow = true;
+    return node;
   }
 }
 
@@ -34,15 +49,27 @@ export class EditorVertex extends EditorObject {
   y: number = 0;
   name: string = '';
 
-  static from_yaml(data: any): EditorVertex {
+  static fromYAML(data: any): EditorVertex {
     let vertex = new EditorVertex();
     vertex.uuid = generate_uuid();
     vertex.x = data[0];
     vertex.y = -data[1];
     vertex.name = data[3];
-    vertex.params_from_yaml(data[4]);
+    vertex.paramsFromYAML(data[4]);
 
     return vertex;
+  }
+
+  toYAML(): YAML.YAMLSeq {
+    let node = new YAML.YAMLSeq();
+    node.add(this.x);
+    node.add(-this.y);
+    node.add(0.0);
+    node.add(this.name);
+    if (this.params.length)
+      node.add(this.paramsToYAML());
+    node.flow = true;
+    return node;
   }
 }
 
@@ -50,71 +77,135 @@ export class EditorWall extends EditorObject {
   start_idx: number = -1;
   end_idx: number = -1;
 
-  static from_yaml(data: any): EditorWall {
+  static fromYAML(data: any): EditorWall {
     let wall = new EditorWall();
     wall.uuid = generate_uuid();
-    wall.params_from_yaml(data[2]);
+    wall.paramsFromYAML(data[2]);
     wall.start_idx = data[0];
     wall.end_idx = data[1];
     return wall;
   }
+
+  toYAML(): YAML.YAMLSeq {
+    let node = new YAML.YAMLSeq();
+    node.add(this.start_idx);
+    node.add(this.end_idx);
+    node.add(this.paramsToYAML());
+    node.flow = true;
+    return node;
+  }
 }
 
-export interface EditorMeasurement extends EditorObject {
-  start_idx: number,
-  end_idx: number,
-  distance: number,
+export class EditorMeasurement extends EditorObject {
+  start_idx: number = -1;
+  end_idx: number = -1;
+  distance: number = 1;
+
+  static fromYAML(data: any): EditorMeasurement {
+    let measurement = new EditorMeasurement();
+    measurement.uuid = generate_uuid();
+    measurement.start_idx = data[0];
+    measurement.end_idx = data[1];
+    measurement.distance = data[2]['distance'][1];
+    return measurement;
+  }
+
+  toYAML(): YAML.YAMLSeq {
+    let node = new YAML.YAMLSeq();
+    node.add(this.start_idx);
+    node.add(this.end_idx);
+    let params_yaml = this.paramsToYAML();
+    params_yaml.add({ key: 'distance', value: [3, this.distance] });
+    node.add(params_yaml);
+    node.flow = true;
+    return node;
+  }
 }
 
 export class EditorLane extends EditorObject {
   start_idx: number = -1;
   end_idx: number = -1;
 
-  static from_yaml(data: any): EditorLane {
+  static fromYAML(data: any): EditorLane {
     let lane = new EditorLane();
     lane.uuid = generate_uuid();
-    lane.params_from_yaml(data[2]);
+    lane.paramsFromYAML(data[2]);
     lane.start_idx = data[0];
     lane.end_idx = data[1];
     return lane;
+  }
+
+  toYAML(): YAML.YAMLSeq {
+    let node = new YAML.YAMLSeq();
+    node.add(this.start_idx);
+    node.add(this.end_idx);
+    node.add(this.paramsToYAML());
+    node.flow = true;
+    return node;
   }
 }
 
 export class EditorFloor extends EditorObject {
   vertex_indices: number[] = [];
 
-  static from_yaml(data: any): EditorFloor {
+  static fromYAML(data: any): EditorFloor {
     let floor = new EditorFloor();
     floor.uuid = generate_uuid();
-    floor.params_from_yaml(data['parameters']);
+    floor.paramsFromYAML(data['parameters']);
     floor.vertex_indices = data['vertices'].map((vertex_idx: number) => vertex_idx);
     return floor;
   }
 }
 
-export interface EditorImage extends EditorObject {
-  filename: string,
-  offset: THREE.Vector3,
-  yaw: number,
-  scale: number,
-  isLegacyDefaultImage: boolean,
+export class EditorImage extends EditorObject {
+  filename: string = '';
+  offset: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
+  yaw: number = 0;
+  scale: number = 1;
+  isLegacyDefaultImage: boolean = false;
 }
 
 export class EditorConstraint extends EditorObject {
   ids: string[] = [];
 
-  static from_yaml(data: any): EditorConstraint {
+  static fromYAML(data: any): EditorConstraint {
     let constraint = new EditorConstraint();
     constraint.uuid = generate_uuid();
     constraint.ids = [ data['ids'][0], data['ids'][1] ];
     return constraint;
   }
+
+  toYAML(): YAML.YAMLMap {
+    let node = new YAML.YAMLMap();
+    node.add({ key: 'ids', value: this.ids });
+    node.flow = true;
+    return node;
+  }
 }
 
-export interface EditorFeature extends EditorObject {
-  name: string,
-  x: number,
-  y: number,
+export class EditorFeature extends EditorObject {
+  name: string = '';
+  x: number = 0;
+  y: number = 0;
+
+  static fromYAML(data: any): EditorFeature {
+    let feature = new EditorFeature();
+    feature.uuid = data['id'];
+    feature.name = data['name'];
+    feature.x = data['x'];
+    feature.y = data['y'];
+    return feature;
+  }
+
+  toYAML(): YAML.YAMLMap {
+    let node = new YAML.YAMLMap();
+    node.add({ key: 'id', value: this.uuid });
+    node.add({ key: 'name', value: this.name });
+    node.add({ key: 'x', value: this.x });
+    node.add({ key: 'y', value: this.y });
+    node.flow = true;
+    return node;
+  }
 }
 
 export class EditorLevel extends EditorObject {
@@ -129,17 +220,45 @@ export class EditorLevel extends EditorObject {
   features: EditorFeature[] = [];
   constraints: EditorConstraint[] = [];
 
-  static from_yaml(_name: string, data: any): EditorLevel {
+  static fromYAML(_name: string, data: any): EditorLevel {
     let level = new EditorLevel();
     level.uuid = generate_uuid();
     level.name = _name;
     level.elevation = data['elevation'];
-    level.constraints = data['constraints'].map((constraint: any) => EditorConstraint.from_yaml(constraint));
-    level.vertices = data['vertices'].map((vertex: any) => EditorVertex.from_yaml(vertex));
-    level.lanes = data['lanes'].map((lane: any) => EditorLane.from_yaml(lane));
-    level.floors = data['floors'].map((floor: any) => EditorFloor.from_yaml(floor));
-    level.walls = data['walls'].map((wall: any) => EditorWall.from_yaml(wall));
+    level.constraints = data['constraints'].map((constraint: any) => EditorConstraint.fromYAML(constraint));
+    level.features = data['features'].map((feature: any) => EditorFeature.fromYAML(feature));
+    level.floors = data['floors'].map((floor: any) => EditorFloor.fromYAML(floor));
+    level.lanes = data['lanes'].map((lane: any) => EditorLane.fromYAML(lane));
+    level.measurements = data['measurements'].map((measurement: any) => EditorMeasurement.fromYAML(measurement));
+    level.vertices = data['vertices'].map((vertex: any) => EditorVertex.fromYAML(vertex));
+    level.walls = data['walls'].map((wall: any) => EditorWall.fromYAML(wall));
+
+    if (data['drawing'] && data['drawing']['filename']) {
+      let image = new EditorImage();
+      image.uuid = generate_uuid();
+      image.filename = data['drawing']['filename'];
+      image.isLegacyDefaultImage = true;
+      level.images.push(image);
+    }
+
     return level;
+  }
+
+  toYAML(): YAML.YAMLMap {
+    let node = new YAML.YAMLMap();
+    if (this.images.length > 0 && this.images[0].isLegacyDefaultImage) {
+      node.add({ key: 'drawing', value: { 'filename': this.images[0].filename } });
+    }
+    node.add({ key: 'constraints', value: this.constraints.map(constraint => constraint.toYAML()) });
+    node.add({ key: 'elevation', value: this.elevation });
+    node.add({ key: 'features', value: this.features.map(feature => feature.toYAML()) });
+    node.add({ key: 'flattened_x_offset', value: 0 });
+    node.add({ key: 'flattened_y_offset', value: 0 });
+    node.add({ key: 'lanes', value: this.lanes.map(lane => lane.toYAML()) });
+    node.add({ key: 'measurements', value: this.measurements.map(measurement => measurement.toYAML()) });
+    node.add({ key: 'vertices', value: this.vertices.map(vertex => vertex.toYAML()) });
+    node.add({ key: 'walls', value: this.walls.map(wall => wall.toYAML()) });
+    return node;
   }
 }
 
@@ -147,16 +266,28 @@ export class EditorBuilding extends EditorObject {
   name: string = '';
   levels: EditorLevel[] = [];
 
-  static from_yaml(yaml_text: string): EditorBuilding {
+  static fromYAML(yaml_text: string): EditorBuilding {
     const yaml = YAML.parse(yaml_text);
     let building = new EditorBuilding();
     building.uuid = generate_uuid();
     building.name = yaml['name'];
     for (const level_name in yaml['levels']) {
       const level_data = yaml['levels'][level_name];
-      building.levels.push(EditorLevel.from_yaml(level_name, level_data));
+      building.levels.push(EditorLevel.fromYAML(level_name, level_data));
     }
     return building;
+  }
+
+  toYAMLString(): string {
+    let yaml_doc = new YAML.Document(new YAML.YAMLMap());
+    let levels_node = new YAML.YAMLMap();
+    for (const level of this.levels) {
+      levels_node.add({ key: level.name, value: level.toYAML() });
+    }
+    yaml_doc.add({ key: 'levels', value: levels_node });
+    yaml_doc.add({ key: 'lifts', value: {} });
+    yaml_doc.add({ key: 'name', value: this.name });
+    return yaml_doc.toString({lineWidth: 0, minContentWidth: 0});
   }
 }
 
--- traffic-editor-pwa/src/YAMLParser.ts
+++ traffic-editor-pwa/src/YAMLParser.ts
@@ -1,73 +1,10 @@
 import {
   CameraPose,
   EditorBuilding,
-  EditorFeature,
-  EditorFloor,
-  EditorImage,
-  EditorLane,
-  EditorLevel,
-  EditorMeasurement,
-  EditorParam,
-  EditorVertex,
-  EditorWall,
   useStore,
 } from './EditorStore'
 import YAML from 'yaml'
-import { v4 as generate_uuid } from 'uuid'
 import * as THREE from 'three'
-
-/*
-function FeatureFactory(data: any): EditorFeature {
-  return {
-    uuid: data['id'],
-    params: [],
-    name: data['name'],
-    x: data['x'],
-    y: data['y'],
-  };
-}
-
-function MeasurementFactory(data: any): EditorMeasurement {
-  return {
-    uuid: generate_uuid(),
-    params: [],
-    start_idx: data[0],
-    end_idx: data[1],
-    distance: data[2]['distance'][1],
-  };
-}
-
-function LevelFactory(level_name: string, level_data: any): EditorLevel {
-  let images = new Array<EditorImage>();
-  if (level_data['drawing'] && level_data['drawing']['filename']) {
-    let image: EditorImage = {
-      filename: level_data['drawing']['filename'],
-      offset: new THREE.Vector3(0, 0, 0),
-      yaw: 0,
-      scale: 1,
-      isLegacyDefaultImage: true,
-      uuid: generate_uuid(),
-      params: Array<EditorParam>(),
-    };
-    images.push(image);
-  }
-
-  return {
-    uuid: generate_uuid(),
-    name: level_name,
-    elevation: level_data['elevation'],
-    params: Array<EditorParam>(),
-    images: images,
-    constraints: level_data['constraints'].map((constraint: any) => ConstraintFactory(constraint)),
-    features: level_data['features'].map((feature: any) => FeatureFactory(feature)),
-    vertices: level_data['vertices'].map((vertex: any) => VertexFactory(vertex)),
-    walls: level_data['walls'].map((wall: any) => WallFactory(wall)),
-    measurements: level_data['measurements'].map((measurement: any) => MeasurementFactory(measurement)),
-    floors: level_data['floors'].map((floor: any) => FloorFactory(floor)),
-    lanes: level_data['lanes'].map((lane: any) => EditorLane.from_yaml(lane)),
-  };
-}
-*/
 
 function computeBoundingBox(building: EditorBuilding): THREE.Box3 {
   let vec_min = new THREE.Vector3(Infinity, Infinity, Infinity);
@@ -105,7 +42,7 @@ export function computeInitialCameraPose(building: EditorBuilding): CameraPose {
 }
 
 export function YAMLParser(yaml_text: string): void {
-  const building = EditorBuilding.from_yaml(yaml_text); //BuildingFactory(yaml_text);
+  const building = EditorBuilding.fromYAML(yaml_text);
   const cameraInitialPose = computeInitialCameraPose(building);
 
   useStore.setState({
@@ -126,99 +63,11 @@ export async function YAMLRetrieveDemo(name: string): Promise<void> {
     process.env.PUBLIC_URL + `/demos/${name}/${name}.building.yaml`);
 }
 
-function ParamArrayYAML(params: EditorParam[]): YAML.Node {
-  let node = new YAML.YAMLMap();
-  for (const param of params) {
-    let param_value_node = new YAML.YAMLSeq();
-    param_value_node.add(param.type_idx);
-    param_value_node.add(param.value);
-    param_value_node.flow = true;
-    node.add({ key: param.name, value: param_value_node });
-  }
-  node.flow = true;
-  return node;
-}
-
-function VertexYAML(vertex: EditorVertex): YAML.Node {
-  let node = new YAML.YAMLSeq();
-  node.add(vertex.x);
-  node.add(-vertex.y);
-  node.add(0.0);
-  node.add(vertex.name);
-  if (vertex.params.length)
-    node.add(ParamArrayYAML(vertex.params));
-  node.flow = true;
-  return node;
-}
-
-function WallYAML(wall: EditorWall): YAML.Node {
-  let node = new YAML.YAMLSeq();
-  node.add(wall.start_idx);
-  node.add(wall.end_idx);
-  node.add(ParamArrayYAML(wall.params));
-  node.flow = true;
-  return node;
-}
-
-function FeatureYAML(feature: EditorFeature): YAML.Node {
-  let node = new YAML.YAMLMap();
-  node.add({ key: 'id', value: feature.uuid });
-  node.add({ key: 'name', value: feature.name });
-  node.add({ key: 'x', value: feature.x });
-  node.add({ key: 'y', value: feature.y });
-  node.flow = true;
-  return node;
-}
-
-function MeasurementYAML(measurement: EditorMeasurement): YAML.Node {
-  let node = new YAML.YAMLSeq();
-  node.add(measurement.start_idx);
-  node.add(measurement.end_idx);
-  const dummy_dist_param: EditorParam = {
-    name: 'distance',
-    uuid: 'dummy',
-    type_idx: 3,
-    value: measurement.distance,
-  };
-  node.add(ParamArrayYAML(measurement.params.concat(dummy_dist_param)));
-  node.flow = true;
-  return node;
-}
-
-function LevelYAML(level: EditorLevel): YAML.Node {
-  let node = new YAML.YAMLMap();
-  let vertices_node = new YAML.YAMLSeq();
-  for (const vertex of level.vertices) {
-    vertices_node.add(VertexYAML(vertex));
-  }
-  if (level.images.length > 0 && level.images[0].isLegacyDefaultImage) {
-    node.add({ key: 'drawing', value: { 'filename': level.images[0].filename } });
-  }
-  node.add({ key: 'elevation', value: level.elevation });
-  node.add({ key: 'features', value: level.features.map((feature) => FeatureYAML(feature)) });
-  node.add({ key: 'measurements', value: level.measurements.map((measurement) => MeasurementYAML(measurement)) });
-  node.add({ key: 'vertices', value: vertices_node });
-  node.add({ key: 'walls', value: level.walls.map((wall) => WallYAML(wall)) });
-  return node;
-}
-
-function BuildingYAMLString(building: EditorBuilding): string {
-  let yaml_doc = new YAML.Document(new YAML.YAMLMap());
-  let levels_node = new YAML.YAMLMap();
-  for (const level of building.levels) {
-    levels_node.add({ key: level.name, value: LevelYAML(level) });
-  }
-  yaml_doc.add({ key: 'levels', value: levels_node });
-  yaml_doc.add({ key: 'lifts', value: {} });
-  yaml_doc.add({ key: 'name', value: building.name });
-  return yaml_doc.toString({lineWidth: 0, minContentWidth: 0});
-}
-
 export async function YAMLSender(url: string): Promise<void> {
   Object.getPrototypeOf(YAML.YAMLMap).maxFlowStringSingleLineLength = 10000;
   console.log('saving: ' + url);
   const { building } = useStore.getState();
-  let yaml_text: string = BuildingYAMLString(building);
+  let yaml_text: string = building.toYAMLString();
   let yaml_size = new Blob([yaml_text]).size;  // utf-8 encoding length
   console.log('  content-length: ' + yaml_size.toString());
   await fetch(url, {
